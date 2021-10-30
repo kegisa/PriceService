a80ad91944ff7ca4aa9b1116b6d37fef7385e605
vlevin <vlevin@azbukavkusa.ru>
2021-10-30 12:18:20 +0400

    Added swager

--- src/main/java/com/victorlevin/PriceService/PriceServiceApplication.java
+++ src/main/java/com/victorlevin/PriceService/PriceServiceApplication.java
@@ -2,8 +2,6 @@ package com.victorlevin.PriceService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.map.repository.config.EnableMapRepositories;
-import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 
 @SpringBootApplication
 public class PriceServiceApplication {
--- src/main/java/com/victorlevin/PriceService/repository/StockRepository.java
+++ src/main/java/com/victorlevin/PriceService/repository/StockRepository.java
@@ -3,7 +3,5 @@ package com.victorlevin.PriceService.repository;
 import com.victorlevin.PriceService.domain.Stock;
 import org.springframework.data.keyvalue.repository.KeyValueRepository;
 
-import java.util.List;
-
 public interface StockRepository extends KeyValueRepository<Stock, String> {
 }
